7448a12fd8a28851e7caa06f8b6eb29bfeec21f7
Suvi <sumaya.shahoyi@gmail.com>
2021-05-14 12:27:02 +0300

    yess

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -20,8 +20,8 @@ const RESOURCES = {
 "favicon.png": "cec06261144dd5f33864440510a04c82",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "5e9408458768e51877f845a805f697a7",
-"/": "5e9408458768e51877f845a805f697a7",
+"index.html": "7a709fc0490192355dc22dd64e054721",
+"/": "7a709fc0490192355dc22dd64e054721",
 "main.dart.js": "a8c091ea09836e0bbd8714031411acbd",
 "manifest.json": "71f434e08409bbbd40da5e41eea58e2c",
 "version.json": "899e23a26fc3de6f9e2fd02ba0c9d9f8"
